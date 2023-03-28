a54b826a229008cd95a14ea171f5081dadd805e9
Denys <86122979+Swartalfheim@users.noreply.github.com>
2023-03-28 18:26:39 +0300

    new_hotfix

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -2,10 +2,8 @@
 
 namespace HW1
 {
-    internal class Program
+    public class Program
     {
-        
-
         static void Main(string[] args)
         {
             Console.WriteLine("Натисьнiть цифру яка вiдповiдає завданню: 1-2");
@@ -14,98 +12,14 @@ namespace HW1
             {
                 case 1:
 
-                    Console.WriteLine("Введiть вишину масиву");
-                    int height = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Введiть довжинуо масиву");
-                    int width = int.Parse(Console.ReadLine());
-                    int[,] snail = new int[height, width];
-                    int[] en = new int[height * width];
-                    int num = 1;
-                    int rez = 0;
-                    int side = 1;
-                    if (height < width)
-                    {
-                        int gump = height;
-                        for (int i = 0; i < height; i++)
-                        {
-                            for (int j = 0; j < gump; j++)
-                            {
-                                rez = rez + (1 * side);
-                                en[rez - 1] = num;
-                                num++;
-
-                            }
-                            for (int j = 0; j < gump; j++)
-                            {
-
-                                en[rez + (height * side) - 1] = num;
-                                num++;
-                                rez = rez + (height * side);
-                            }
-                            gump--;
-                            side = side * -1;
-                        }
-                        int n = 0;
-                        for (int x = 0; x < width; x++)
-                        {
-                            for (int y = 0; y < height; y++)
-                            {
-                                snail[y, x] = en[n];
-                                n++;
-                            }
-                        }
-                        Console.WriteLine();
-                        for (int y = 0; y < height; y++)
-                        {
-                            for (int x = 0; x < width; x++)
-                            {
-                                Console.Write($"{snail[y, x]}; ");
-                            }
-                            Console.WriteLine();
-                        }
-                    }
-                    if (width < height)
-                    {
-                        int gump = width;
-                        rez = -1;
-                        for (int i = 0; i < 3; i++)
-                        {
-                            for (int j = 0; j < height; j++)
-                            {
-                                rez = rez + (1 * side);
-                                en[rez] = num;
-                                num++;
-
-                            }
-                            for (int j = 0; j < width - 1; j++)
-                            {
-                                rez = rez + (1 * side);
-                                rez = rez + (gump * side);
-                                en[rez] = num;
-                                num++;
-                            }
-                            height--;
-                            width--;
-                            side = side * (-1);
-                        }
-                        int n = 0;
-                        for (int i = 0; i < en.Length; i++)
-                        {
-
-                            Console.Write($"{en[i]}; ");
-
-                        }
-
-                    }
-
-                    break;
-                case 2:
-
-                    int[,] mas;
-                    int rows, columns, colors = 16;
-                    (int i, int j) maxStart = (-1, -1), maxEnd = (-1, -1), start = (-1, -1), pStart = (-1, -1), pEnd = (-1, -1);
+                    
+                    int[,] matrix;
+                    int rows, columns, counter = 10;
                     bool result;
-                    Random random = new Random();
+
+                    
+                    int r = 0, c = 0, maxRowSteps, maxColumnSteps, rowSteps = 0, columnSteps = 0;
+                    (int r, int c) direction = (1, 0);
 
                     do
                     {
@@ -118,10 +32,84 @@ namespace HW1
                         result = int.TryParse(Console.ReadLine(), out columns);
                     } while (!result || columns <= 0);
 
-                    mas = new int[rows, columns];
+                    matrix = new int[rows, columns];
+                    maxRowSteps = rows - 1;
+                    maxColumnSteps = columns - 1;
+
+                    while (maxRowSteps > 0 || maxColumnSteps > 0)
+                    {
+                        matrix[r, c] = counter++;
+                       
+                        if ((rowSteps >= maxRowSteps && maxRowSteps > 0) || (columnSteps >= maxColumnSteps && maxColumnSteps > 0))
+                        {                            
+                            rowSteps = 0;
+                            columnSteps = 0;
+                            switch (direction)
+                            {
+                                case (1, 0): 
+                                    direction = (0, 1);
+                                    if (maxColumnSteps != columns - 1) 
+                                    {
+                                        maxRowSteps--;
+                                    }
+                                    break;
+                                case (0, 1): 
+                                    direction = (-1, 0); 
+                                    maxColumnSteps--;
+                                    break;
+                                case (-1, 0): 
+                                    direction = (0, -1); 
+                                    maxRowSteps--;
+                                    break;
+                                case (0, -1):
+                                    direction = (1, 0); 
+                                    maxColumnSteps--;
+                                    break;
+                            }
+                            if (rows > columns && maxRowSteps < 2)
+                            {
+                                maxRowSteps--;
+                            }
+                        }
+                        r += direction.r;
+                        c += direction.c;
+                        rowSteps += Math.Abs(direction.r);
+                        columnSteps += Math.Abs(direction.c);
+                    }
+
                     for (int i = 0; i < rows; i++)
                     {
                         for (int j = 0; j < columns; j++)
+                        {
+                            Console.Write($"{matrix[i, j]} ");
+                        }
+                        Console.WriteLine();
+                    }
+
+                    break;
+                case 2:
+
+                    int[,] mas;
+                    int rows1, columns1, colors = 16;
+                    (int i, int j) maxStart = (-1, -1), maxEnd = (-1, -1), start = (-1, -1), pStart = (-1, -1), pEnd = (-1, -1);
+                    bool result1;
+                    Random random = new Random();
+
+                    do
+                    {
+                        Console.Write("Введiть кiлькiсть рядкiв: ");
+                        result1 = int.TryParse(Console.ReadLine(), out rows1);
+                    } while (!result1 || rows1 <= 0);
+                    do
+                    {
+                        Console.Write("Введiть кiлькiсть стовбцiв: ");
+                        result = int.TryParse(Console.ReadLine(), out columns1);
+                    } while (!result1 || columns1 <= 0);
+
+                    mas = new int[rows1, columns1];
+                    for (int i = 0; i < rows1; i++)
+                    {
+                        for (int j = 0; j < columns1; j++)
                         {
                             mas[i, j] = random.Next(colors + 1);
                             Console.Write($"{mas[i, j]}; ");
@@ -129,9 +117,9 @@ namespace HW1
                         Console.WriteLine();
                     }
 
-                    for (int i = 0; i < rows; i++)
+                    for (int i = 0; i < rows1; i++)
                     {
-                        for (int j = 0; j < columns; j++)
+                        for (int j = 0; j < columns1; j++)
                         {
                             if ((j == 0) || (j > 0 && mas[i, j] != mas[i, j - 1]))
                             {
@@ -139,7 +127,7 @@ namespace HW1
                                 pEnd = j == 0 ? (-1, -1) : (i, j - 1);
                                 start = (i, j);
                             }
-                            if ((j >= columns - 1) && mas[i, j] == mas[i, j - 1])
+                            if ((j >= columns1 - 1) && mas[i, j] == mas[i, j - 1])
                             {
                                 pStart = start;
                                 pEnd = (i, j);
