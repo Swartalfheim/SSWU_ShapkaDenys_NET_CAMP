72f80e7db03aee7e4a32942fcde7c22b11b459c1
senivlm <91743906+senivlm@users.noreply.github.com>
2023-04-11 15:00:12 +0300

    Update Program.cs

--- Home_Work3/HW3/Program.cs
+++ Home_Work3/HW3/Program.cs
@@ -1,7 +1,7 @@
-﻿namespace HW3
-{
+вежа має 
     internal class Program
-    {
+    {// У Вас вежа має користувачів. Я думаю, краще користувач використовує вежу. А це інша стрілка.
+        
         public static void Main(string[] args)
         {
             PrintInfo printInfo = new PrintInfo();
