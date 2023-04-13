86e8d8612c60514522cf867eff03998e2249be39
Denys <86122979+Swartalfheim@users.noreply.github.com>
2023-04-13 12:02:53 +0300

    add_HW5

---
+++ Home_Work5/Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
