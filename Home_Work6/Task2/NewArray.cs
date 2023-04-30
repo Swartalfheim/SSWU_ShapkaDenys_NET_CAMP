0d47260ca35a56dee75391262bc1ea8f63e5e8b6
Denys <86122979+Swartalfheim@users.noreply.github.com>
2023-04-30 21:57:29 +0300

    fix

--- Home_Work6/Task2/NewArray.cs
+++ Home_Work6/Task2/NewArray.cs
@@ -8,20 +8,71 @@ namespace Task2
 {
     public class NewArray
     {
-        public IEnumerable<int> NewNumbers(params int[][] mas)
+        public IEnumerable<int> NewNumbers(params int[][] arrays)
         {
-            List<int> numbers = new List<int>();
-            foreach (var item in mas)
-            {
-                numbers.AddRange(item);
-            }
+            int[] mins = new int[arrays.Length];
+            int max = arrays.Select(m => m.Max()).Max();
+            List<int> sorted = new List<int>(); // to check if a minimum was already output
+            List<int> sortedMins = new List<int>(); // to find next minimums in all arrays
 
-            numbers.Sort();
-
-            foreach (var item in numbers)
+            do
             {
-                yield return item;
+                if (sorted.Any())
+                {
+                    for (int i = 0; i < arrays.Length; i++)
+                    {
+                        int currMin = mins[i];
+                        for (int j = 0; j < arrays[i].Length; j++)
+                        {
+                            if (arrays[i][j] > mins[i])
+                            {
+                                currMin = arrays[i][j];
+                                break;
+                            }
+                        }
+
+                        for (int j = 0; j < arrays[i].Length; j++)
+                        {
+                            if (arrays[i][j] > mins[i] && currMin > arrays[i][j])
+                            {
+                                currMin = arrays[i][j];
+                            }
+                        }
+
+                        mins[i] = currMin;
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < arrays.Length; i++)
+                    {
+                        mins[i] = arrays[i].Min();
+                    }
+                }
+
+                sortedMins = new List<int>(mins.Distinct().OrderBy(m => m));
+                foreach (int min in sortedMins)
+                {
+                    if (sorted.Contains(min))
+                    {
+                        continue;
+                    }
+
+                    for (int i = 0; i < arrays.Length; i++)
+                    {
+                        for (int j = 0; j < arrays[i].Length; j++)
+                        {
+                            if (arrays[i][j] == min)
+                            {
+                                yield return arrays[i][j];
+                            }
+                        }
+                    }
+                }
+
+                sorted.AddRange(sortedMins);
             }
+            while (sortedMins.Max() != max);
         }
     }
 }
