267c8a45433b569ab308785242f6ee0b9612197f
senivlm <91743906+senivlm@users.noreply.github.com>
2023-05-02 13:46:13 +0300

    Update UniqueWords.cs

--- Home_Work6/Task3/UniqueWords.cs
+++ Home_Work6/Task3/UniqueWords.cs
@@ -9,7 +9,7 @@ namespace Task3
     public class UniqueWords
     {
         public IEnumerable<string> Uniquewords(string text)
-        {
+        {// Не враховано, що пробільні символи можуть бути різні. І їх може бути кілька підряд.
             string[] words = text.ToLower().Split(',', '-', '.', ' ');
             var hashSet = new HashSet<string>();
 
@@ -21,12 +21,12 @@ namespace Task3
                 }
             }
         }
-
+// До умови задачі нам це не потрібно
         public IEnumerable<string> NUniquewords(string text)
-        {
+        {// Ви змінили колекцію
             string[] words = text.ToLower().Split(',', '-', '.', '“', '”', ' ');
             foreach (var item in words)
-            {
+            {// Не ефективно тут
                 if (words.Where(q => q == item).Count() == 1)
                 {
                     yield return item;
